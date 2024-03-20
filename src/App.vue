ab7cb79f22af7ad2aa61096d97dfc47946814694
BUERKJU <julia.buerk@mercedes-benz.com>
2024-03-20 15:00:48 +0100

    setup header layout

--- src/App.vue
+++ src/App.vue
@@ -5,34 +5,39 @@ import Home from './components/Home.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/map">Map</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
-    </div>
   </header>
 
-  <RouterView />
+  <RouterView style="margin-bottom: 50px;" />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+RouterLink {
+  color: var(--color-text);
+  text-decoration: none;
+  padding: 1rem;
+  border-left: 1px solid var(--color-border);
+  font-size: 18px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 1;
+  background-color: #cc503b;
+  align-items: center;
+  padding:1rem;
 }
 
 nav {
   width: 100%;
-  font-size: 12px;
+  font-size: 18px;
   text-align: center;
   margin-top: 2rem;
 }
@@ -55,31 +60,5 @@ nav a:first-of-type {
   border: 0;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
 ./components/Home.vue
