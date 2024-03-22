8a349236a8a3be457736f92d6b2c5455d68b1a6c
julia.buerk <jujuju.ok.256@gmail.com>
2024-03-22 12:31:44 +0100

    Add Map basic

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,25 +1,39 @@
-import '@googlemaps/extended-component-library/overlay_layout.js';
-
 <template>
-    <div class="map">
-        <h1>Anxiety Map</h1>
-
-        <iframe width="600" height="450" style="border:0" loading="lazy" allowfullscreen
-            referrerpolicy="no-referrer-when-downgrade" 
-            src="https://www.google.com/maps/embed/v1/place?key=API_KEY
-            &q=Space+Needle,Seattle+WA">
-        </iframe>
-        <!--https://developers.google.com/maps/documentation/embed/embedding-map?hl=de-->
+  <div>
+    <h1>Anxiety Map</h1>
+    <div style="height:600px; width:800px">
+      <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
+        <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base"
+          name="OpenStreetMap"></l-tile-layer>
+      </l-map>
     </div>
-
+  </div>
 </template>
+
+
+<script>
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+  },
+  data() {
+    return {
+      zoom: 2,
+    };
+  },
+};
+</script>
 
 <style>
 @media (min-width: 1024px) {
-    .map {
-        min-height: 100vh;
-        display: flex;
-        align-items: center;
-    }
+  .map {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
 }
 </style>
