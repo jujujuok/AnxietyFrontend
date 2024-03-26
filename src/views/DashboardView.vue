618d82f64bde4fd3ad09fc0db67b5029abf513e1
BUERKJU <julia.buerk@mercedes-benz.com>
2024-03-26 18:34:28 +0100

    Update DashboardView component

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,72 +1,92 @@
 <script setup lang="ts">
+import type { PropType } from 'vue'   // the fck is this?!
+import { defineComponent } from 'vue'
 
 // --- MATERIAL IMPORTS ---
-// import {md-list-item, md-list} from '@material/list';
+import '@material/web/list/list'
+import '@material/web/list/list-item'
 
 // --- ICONS ---
 // import WeatherIcon from './icons/IconWeather.vue'
 
 // --- COMPONENTS ---
-import DashboardEntry from '@/components/dashboard/DashboardEntry.vue'
+import DashboardEntry from '@/components/DashboardEntry.vue';
 
-const entries = {
-  natural_disasters:
-  {
-    // purple
-    color: "#800080",
-    icon: "weather-icon",
-    title: "Natural Disasters",
-    data: {
-      "Deutschland": "Hochwasser",
-      "Panama": "Erdbeben",
-      "Stuttgart": "its raining, man",
-      "München": "flood"
-    }
+abstract class DashboardItemStruct {
+  color: string = "#aaaaaa"
+  icon: string = "default-icon"
+  iid: number
+  title: string
+  date: Date
+  description: string
+  location: string
 
-  },
-  interpol_red_notice: {
-    color: "#aa2020",
-    icon: "red-notice-icon",
-    title: "Interpol Red Notice",
-    data: {
-      "Deutschland": "Kevin, 20 Jahre, zuletzt gesehen beim Kuchen essen",
-      "Panama": "Maximilian bvs, 19 Jahre, Steuerhinterziehung",
-    }
+  constructor(iid: number, title: string, date: Date, description: string, location: string) {
+    this.iid = iid
+    this.title = title
+    this.date = date
+    this.description = description
+    this.location = location
   }
-};
+}
+
+class WeatherStruct extends DashboardItemStruct {
+  constructor(iid: number, title: string, date: Date, description: string, location: string) {
+    super(iid, title, date, description, location)
+    this.color = "#800080"
+    this.icon = "weather-icon"
+  }
+}
+
+class InterpolRedNoticeStruct extends DashboardItemStruct {
+  constructor(iid: number, title: string, date: Date, description: string, location: string) {
+    super(iid, title, date, description, location)
+    this.color = "#aa2020"
+    this.icon = "red-notice-icon"
+  }
+}
+
+
+const entries = [
+  new WeatherStruct(1, "Hochwasser", new Date(), "Es regnet in Deutschland", "Deutschland"),
+  new WeatherStruct(2, "Erdbeben", new Date(), "Es regnet in Panama", "Panama"),
+  new InterpolRedNoticeStruct(3, "Kevin", new Date(), "20 Jahre, zuletzt gesehen beim Kuchen essen", "Deutschland"),
+  new InterpolRedNoticeStruct(4, "Maximilian bvs", new Date(), "19 Jahre, Steuerhinterziehung", "Panama")
+];
+
+// export interface RedNoticeDetails extends Record<string, any> {
+//   iid: number
+//   type: string
+//   title: string
+//   date: Date
+//   description: string
+//   location: string
+// }
+// TODO: Fabi warum funktioniert export default nicht
 </script>
 
 <template>
   <div class="dashboard">
     <h1>Dashboard</h1>
-    <DashboardEntry v-for="(entry, key) in entries" :key="key" :entry="entry" />
-  </div>
 
-  <md-list style="max-width: 300px;">
-    <md-list-item>
-      Fruits
-    </md-list-item>
-    <md-divider></md-divider>
-    <md-list-item>
-      Apple
-    </md-list-item>
-    <md-list-item>
-      Banana
-    </md-list-item>
-    <md-list-item>
-      <div slot="headline">Cucumber</div>
-      <div slot="supporting-text">Cucumbers are long green fruits that are just as long as this multi-line description
-      </div>
-    </md-list-item>
-    <md-list-item interactive href="https://google.com/search?q=buy+kiwis&tbm=shop" target="_blank">
-      <div slot="headline">Shop for Kiwis</div>
-      <div slot="supporting-text">This will link you out in a new tab</div>
-      <md-icon slot="end">open_in_new</md-icon>
-    </md-list-item>
-  </md-list>
+    <md-list v-if="entries"> <!-- todo: if there are entries -->
+      <DashboardEntry v-for="entry in entries" 
+      :key="entry.iid" 
+      :color="entry.color" 
+      :icon="entry.icon" 
+      :iid="entry.iid"
+      :title="entry.title" 
+      :date="entry.date" 
+      :description="entry.description"
+      :location="entry.location" />
+    </md-list>
+    <div v-else>
+      <p>Keine Katastrophe vorhanden</p>
+    </div>
+  </div>
 </template>
 
-<style>
+<style scoped>
 .dashboard {
   display: flex;
   gap: 1rem;
@@ -84,17 +104,29 @@ const entries = {
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
 
-:root {
-  background-color: #f4fbfa;
-  --md-list-container-color: #f4fbfa;
-  --md-list-item-label-text-color: #161d1d;
-  --md-list-item-leading-icon-color: #006a6a;
-  --md-list-item-trailing-icon-color: #006a6a;
-  --md-list-item-leading-icon-size: 20px;
-  --md-list-item-trailing-icon-size: 20px;
+div {
+  display: grid;
+  place-items: center;
 }
 
-md-list-item.unhealthy {
-  --md-list-item-trailing-icon-color: #ba1a1a;
+p {
+  font-size: 0.8rem;
+  font-family: 'Roboto Mono', monospace;
 }
 </style>
+
+<!--
+  <script lang="ts">
+import type { PropType } from 'vue'
+import { defineComponent } from 'vue'
+
+import '@material/web/list/list'
+import '@material/web/list/list-item'
+import '@material/web/icon/icon'
+import ResultListItem from '@/components/search-result/ResultListItem.vue'
+import type { SearchResult } from '@/views/SearchQuery'
+import { useMedia } from '@/Media'
+
+
+</script>
+-->
