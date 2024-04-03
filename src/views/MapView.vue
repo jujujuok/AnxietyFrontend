058feb6859534b96c071c2ecaa13037a5d56816d
jujujuok <jujuju.ok.256@gmail.com>
2024-04-03 15:04:58 +0200

    Add polygon functions with examples

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -16,15 +16,50 @@ import { ref, onMounted } from 'vue';
 import "leaflet/dist/leaflet.css";
 import * as L from 'leaflet';
 
-const initialMap = ref(null);
+var latlngs = [
+  [51.1657, 10.4515],
+  [52.5200, 13.4050],
+  [48.7758, 9.1829],
+  [50.1109, 8.6821],
+  [53.5511, 9.9937]
+];
 
-onMounted(()=> {
-    initialMap.value = L.map('map').setView([51.163361, 10.447683], 6);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19, 
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(initialMap.value);
+var latlngs2 = [
+  [
+    8.3203,
+    48.5886
+  ],
+  [
+    8.209,
+    48.4666
+  ],
+  [
+    8.2159,
+    48.4568
+  ],
+  [
+    8.1311,
+    48.6068
+  ]
+].map(array => array.reverse())
+
+
+const map = ref(null);
+
+onMounted(() => {
+  map.value = L.map('map').setView([51.163361, 10.447683], 6);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map.value);
+
+  L.marker([50.0000, 8.6821]).addTo(map.value);
+  L.marker([49.1234, 8.6821],).addTo(map.value);
+
+  L.polygon(latlngs2, { color: 'blue' }).addTo(map.value);
+  L.polygon(latlngs, { color: 'red' }).addTo(map.value);
 });
+
 </script>
 
 
