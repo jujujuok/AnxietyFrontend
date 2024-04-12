bb3da90b0b976eab4ccf0b5f56499c08b3ffbcc1
KevinBasener <120453853+KevinBasener@users.noreply.github.com>
2024-04-12 13:47:55 +0200

    Add vuetify

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,30 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+//Oh-Vue-Icons
 import { OhVueIcon, addIcons } from 'oh-vue-icons'
-import { CoHome, CoMap, RiDashboard2Line } from 'oh-vue-icons/icons'
+import { CoHome, CoMap, RiDashboard2Line, IoLocation } from 'oh-vue-icons/icons'
+
 addIcons(CoMap)
 addIcons(RiDashboard2Line)
 addIcons(CoHome)
+addIcons(IoLocation)
 
 const app = createApp(App)
 
 app.component('VIcon', OhVueIcon)
 
 app.use(router)
+app.use(vuetify)
 app.mount('#app')
