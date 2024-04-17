df23e70410217274c4fe05a6b962b80dd86d76e0
julia.buerk <julia.buerk@dhbw-engineering.de>
2024-04-17 17:16:01 +0200

    add world map

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MapView from '../views/MapView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,7 +13,12 @@ const router = createRouter({
     {
       path: '/map',
       name: 'map',
-      component: () => import('../views/MapView.vue')
+      component: () => MapView
+    },
+    {
+      path: '/world',
+      name: 'world',
+      component: () => MapView
     },
     {
       path: '/dashboard',
