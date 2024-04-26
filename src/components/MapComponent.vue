f9c61934d3fe232a7d18974ffde444d0fd8c7592
julia.buerk <julia.buerk@dhbw-engineering.de>
2024-04-26 18:28:20 +0200

    extract map and world map components

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -7,8 +7,9 @@ import * as L from 'leaflet';
 let props = defineProps<{
   start_lon: number
   start_lat: number
+  zoom_start: number
   pins?: Array<number>
-  areas?: Array<number>
+  areas?: []
   title?: string
   likes?: number
 }>();
@@ -17,7 +18,7 @@ const map = ref({} as L.Map);
 
 
 onMounted(() => {
-  map.value = L.map('map').setView([props.start_lon, props.start_lat], 6);
+  map.value = L.map('map').setView([props.start_lon, props.start_lat], props.zoom_start);
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
@@ -29,19 +30,19 @@ onMounted(() => {
   // L.polygon(latlngs2, { color: 'blue' }).addTo(map.value);
   // L.polygon(latlngs, { color: 'red' }).addTo(map.value);
 
-  // coordinates.forEach(coords => {
-  //   const polygon = L.polygon(coords, { color: 'red' }).addTo(map.value);
+  props.areas.forEach(coords => {
+    const polygon = L.polygon(coords, { color: 'red' }).addTo(map.value);
 
-  //   polygon.bindPopup('This is a polygon');
+    polygon.bindPopup('This is a polygon');
 
-  //   // // Hover effect
-  //   polygon.on('mouseover', function (e) {
-  //     this.setStyle({ fillOpacity: 0.7 });
-  //   });
-  //   polygon.on('mouseout', function (e) {
-  //     this.setStyle({ fillOpacity: 0.2 });
-  //   });
-  // });
+    // // Hover effect
+    polygon.on('mouseover', function (e) {
+      this.setStyle({ fillOpacity: 0.7 });
+    });
+    polygon.on('mouseout', function (e) {
+      this.setStyle({ fillOpacity: 0.2 });
+    });
+  });
 
 });
 </script>
--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -4,29 +4,6 @@ import { ref } from 'vue';
 import MapComponent from "@/components/MapComponent.vue"
 import FilterComponent from '@/components/FilterComponent.vue'
 import "leaflet/dist/leaflet.css";
-import * as L from 'leaflet';
-
-
-// todo fetch ok fabi 
-
-const url = "http://212.132.100.147:8000/map";
-
-async function callApi(url) {
-  const headers = {
-    'accept': 'application/json'
-  };
-
-  const response = await fetch(url, { headers });
-
-  if (response.ok) {
-    return await response.json();
-  }
-}
-
-let data = callApi(url);
-
-console.log("API DATA", data)
-
 
 const coordinates = [
   [
@@ -44,7 +21,47 @@ const coordinates = [
         50.0
       ]
     ].map(array => array.reverse())
-  ]]
+  ]
+]
+
+
+const url = "http://api.risiko-radar.info/map";
+
+async function callApi(url) {
+  const headers = {
+    'accept': 'application/json'
+  };
+
+  const response = await fetch(url, { headers });
+
+  if (response.ok) {
+    return await response.json();
+  }
+}
+
+callApi(url)
+  .then(data => {
+    // console.log("API DATA", data);
+
+    data.forEach(item => {
+      // console.log("ID:", item.id);
+      // console.log("Type:", item.type);
+      // console.log("Warning:", item.warning);
+      console.log("area:",item.area)
+
+      // coordinates[0][0].push(item.area.map(array => array.reverse()))
+    });
+
+  })
+  .catch(error => {
+    console.error("Error occurred:", error);
+  });
+
+
+console.log("API DATA", data)
+
+
+
 
 const filter_names = ["radioactive", "air", "interpol", "autobahn", "weather_warnings"];
 const filter_icons = ["mdi-radioactive", "mdi-weather-windy", "mdi-police-badge", "mdi-car", "mdi-weather-lightning-rainy"];
@@ -55,7 +72,7 @@ const filter_icons = ["mdi-radioactive", "mdi-weather-windy", "mdi-police-badge"
   <FilterComponent :names="filter_names" :icons="filter_icons" />
 
   <div id="container">
-    <MapComponent :start_lon="51.163361" :start_lat="10.447683" />
+    <MapComponent :start_lon="51.163361" :start_lat="10.447683" :zoom_start="6" :areas="coordinates" />
   </div>
 </template>
 
