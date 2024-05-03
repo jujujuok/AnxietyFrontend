ecc79d202d6bbaefffc3b6b4190365a13d739b47
KevinBasener <120453853+KevinBasener@users.noreply.github.com>
2024-05-03 15:17:08 +0200

    Fix merge

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MapView from '../views/MapView.vue'
+import WorldView from '../views/WorldView.vue'
+import ErrorView from '../views/ErrorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
