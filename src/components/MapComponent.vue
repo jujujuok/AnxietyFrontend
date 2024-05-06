614d65bad271570050fba8413d227c301b3f5990
KevinBasener <120453853+KevinBasener@users.noreply.github.com>
2024-05-06 12:42:07 +0200

    Fixing merge

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -1,107 +1,108 @@
 <script setup>
 
-import { ref, onMounted, watch } from 'vue';
+import {ref, onMounted, watch} from 'vue';
 import "leaflet/dist/leaflet.css";
 import * as L from 'leaflet';
 import DataManager from "@/services/data-manager.js";
 
 let props = defineProps({
-  start_lon: Number,
-  start_lat: Number,
-  zoom_start: Number,
-  url: String,
-  filter: [],
+    start_lon: Number,
+    start_lat: Number,
+    zoom_start: Number,
+    url: String,
+    filter: [],
 })
 const dataManager = new DataManager();
 
 async function callApi(url) {
-  try {
-      const headers = {
-          'accept': 'application/json'
-      };
+    try {
+        const headers = {
+            'accept': 'application/json'
+        };
 
-      const response = await fetch(url, { headers });
+        const response = await fetch(url, {headers});
+        console.log(response);
 
-      if (response.ok) {
-          return await response.json();
-      }
-  } catch(e){
-      console.error('Error while fetching map data: ', e);
-  }
+        if (response.ok) {
+            return await response.json();
+        }
+    } catch (e) {
+        console.error('Error while fetching map data: ', e);
+    }
 }
 
 const map = ref(L.Map);
 
 
 onMounted(async () => {
-  map.value = L.map('map').setView([props.start_lon, props.start_lat], props.zoom_start);
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }).addTo(map.value);
+    map.value = L.map('map').setView([props.start_lon, props.start_lat], props.zoom_start);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map.value);
 
-  updateMap()
+    updateMap()
 });
 
 function updateMap() {
-  if (!props.filter) return;
+    if (!props.filter) return;
 
-  callApi(props.url).then(data => dataManager.saveMapData(data));
-  const data = dataManager.getMapData();
+    callApi(props.url).then(data => dataManager.saveMapData(data));
+    const data = dataManager.getMapData();
 
-  data.forEach(item => {
-    let item_color = 'red';
-    let pop = item.title;
-    let coords = [[item.area.map(innerArray => innerArray.map(coord => coord.reverse()))]];
-    console.log("filter: ", props.filter, "item type: ", item.type)
+    data.forEach(item => {
+        let item_color = 'red';
+        let pop = item.title;
+        let coords = [[item.area.map(innerArray => innerArray.map(coord => coord.reverse()))]];
+        console.log("filter: ", props.filter, "item type: ", item.type)
 
-    switch (item.type) {
-      case "nina":
-        item_color = 'blue';
-        break;
-      case "weather":
-        item_color = 'green';
-        break;
-      case "street_report":
-        item_color = 'purple';
-        break;
-      default:
-        item_color = 'red';
-        break;
-    }
+        switch (item.type) {
+            case "nina":
+                item_color = 'blue';
+                break;
+            case "weather":
+                item_color = 'green';
+                break;
+            case "street_report":
+                item_color = 'purple';
+                break;
+            default:
+                item_color = 'red';
+                break;
+        }
 
-    const polygon = L.polygon(coords, { color: item_color }).addTo(map.value);
-    polygon.bindPopup(pop);
-    polygon.on('mouseover', function (e) {
-      polygon.setStyle({ fillOpacity: 0.7 });
+        const polygon = L.polygon(coords, {color: item_color}).addTo(map.value);
+        polygon.bindPopup(pop);
+        polygon.on('mouseover', function (e) {
+            polygon.setStyle({fillOpacity: 0.7});
+        });
+        polygon.on('mouseout', function (e) {
+            polygon.setStyle({fillOpacity: 0.2});
+        });
     });
-    polygon.on('mouseout', function (e) {
-      polygon.setStyle({ fillOpacity: 0.2 });
-    });
-  });
 }
 
 watch(props, async () => {
-  console.log("watcher");
-  updateMap();
+    console.log("watcher");
+    updateMap();
 });
 </script>
 
 
 <template>
-  <div id="map"></div>
+    <div id="map"></div>
 </template>
 
 <style>
 #map {
-  height: 100%;
+    height: 100%;
 }
 
 @media (min-width: 1024px) {
-  .map {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+    .map {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
 }
 </style>
