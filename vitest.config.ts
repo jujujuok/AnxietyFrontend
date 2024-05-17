605abd39a0a4ddf46804fe19e1f2c754cf19d96d
KevinBasener <120453853+KevinBasener@users.noreply.github.com>
2024-05-17 09:07:42 +0200

    Fix building

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath } from 'url';
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
-import viteConfig from './vite.config.js';
+import viteConfig from './vite.config.ts';
 
 export default mergeConfig(
   viteConfig,
